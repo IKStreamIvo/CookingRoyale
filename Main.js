4b5a37fe5b8f84ae809b483ab3810ca0bbde6db9
IKStreamIvo <ivostreaming@gmail.com>
2017-12-28 14:02:08 +0100

    fixed error

--- Main.js
+++ Main.js
@@ -1,9 +1,11 @@
 var game = new Phaser.Game(1920, 1079, Phaser.AUTO, 'game'), Main = function () {};
 
-preload: function () {
-    game.load.image('background',    'sprites/background.png');
-    game.load.image('loading',  'sprites/loading.png');
-    game.load.image('logo',    'sprites/logo.png');
-    game.load.script('utils',   'lib/Utils.js');
-    game.load.script('splash',  'states/Splash.js');
+Main.prototype = {
+    preload: function () {
+        game.load.image('background',    'sprites/background.png');
+        game.load.image('loading',  'sprites/loading.png');
+        game.load.image('logo',    'sprites/logo.png');
+        game.load.script('utils',   'lib/Utils.js');
+        game.load.script('splash',  'states/Splash.js');
+    }
 }
