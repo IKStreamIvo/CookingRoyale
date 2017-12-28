3668f72358459672c6eb640cc98246338319337d
Marcel <marcelformer@gmail.com>
2017-12-28 14:19:11 +0100

    SplashUpdate
    Splash Updated

--- states/Splash.js
+++ states/Splash.js
@@ -10,17 +10,20 @@ Splash.prototype = {
         //Also loads the script that initializes the fonts.
         game.load.script('WebFont', 'vendor/webfontloader.js');
         game.load.script('PlayGame','states/Playgame.js');
+        console.log('Finished loading scripts');
     },
 
     loadBgm: function () {
         //Loads all background music
         game.load.audio('MenuOST', 'placeholders/Stardust.mp3');
+        console.log('Finished loading audio');
     },
 
     loadImages: function () {
         //Loads all images. If you add images to the game, make sure to add them in here as well!
         // Copy & paste existing code and replace with said images.
         game.load.image('MenuBackground', 'placeholders/menuBG.jpg');
+        console.log('Finished loading background image');
     },
 
     loadFonts: function () {
@@ -39,6 +42,7 @@ Splash.prototype = {
         this.logo       = game.make.sprite(game.world.centerX, 200, 'logo');
         this.status     = game.make.text(game.world.centerX, 380, 'Loading...', {fill: 'white'});
         utils.centerGameObjects([this.logo, this.status]);
+        console.log('Finished initializing splash');
     },
 
     preload: function () {
@@ -52,17 +56,19 @@ Splash.prototype = {
         this.loadImages();
         this.loadFonts();
         this.loadBgm();
-
+        console.log('Finished loading logo and functions');
     },
 
     addGameStates: function () {
         game.state.add("PlayGame",Game);
+        console.log('Finished loading Game script');
     },
 
     addGameMusic: function () {
         music = game.add.audio('MenuOST');
         music.loop = true;
         music.play();
+        console.log('Started playing music');
     },
 
     create: function() {
@@ -73,5 +79,6 @@ Splash.prototype = {
         setTimeout(function () {
             //game.state.start("PlayGame");
         }, 3000);
+        console.log('Done.');
     }
 };
